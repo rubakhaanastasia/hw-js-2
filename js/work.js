87c3f142e831b5acee821771cff47687b2541055
rubakhaanastasia <145857108+rubakhaanastasia@users.noreply.github.com>
2024-10-13 15:40:46 +0200

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,44 @@
+let celsius = 16;
+let farenheit = 50;
+console.log(`${celsius}°C рівняється до ${farenheit}°F`);
+
+const month = 31;
+const hours = month * 24;
+const minutes = hours * 60;
+console.log(
+  `${month} день у місяці дорівнює ${hours} годин або ${minutes} хвилин.`
+);
+
+let health = 4500;
+let energy = 2500;
+let playerDeath = health - 900;
+let playirDeath = energy - 600;
+console.log(playerDeath);
+console.log(playirDeath);
+
+let purchase = 368;
+let discount = purchase * 0.1;
+console.log(discount);
+
+let floatingComa = 13.64;
+let coma = Math.floor(floatingComa);
+console.log(coma);
+
+let storage = "62.46";
+let storageNember = parseFloat(storage);
+console.log(storageNember);
+
+let stringStorage = "9658";
+let stringStorsge = parseInt(stringStorage);
+console.log(stringStorage);
+
+let number = 9;
+let qaderRot = Math.sqrt(number);
+console.log(`корінь з ${number} дорівнює ${qaderRot}`);
+
+const integer = 50;
+const line = "52";
+//метод parseInt() для перетворення рядка у ціле число.
+const conveyor = parseInt(line);
+console.log(line);
+console.log(integer);
